e4446f11aef031783552e20ee701c7b754f815cb
Valerie Ivanova <valeri9233@gmail.com>
2023-07-24 23:11:20 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='taskify',
+    version='',
+    packages=['taskify', 'taskify.common', 'taskify.common.migrations', 'taskify.tasksApp',
+              'taskify.tasksApp.migrations', 'taskify.profileApp', 'taskify.profileApp.migrations'],
+    url='',
+    license='',
+    author='valerie',
+    author_email='',
+    description=''
+)
